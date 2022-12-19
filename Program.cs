434f7cb097ddad9ebe55be8b920bae1d32277a8b
ilyasklf <104015535+ilyasklf@users.noreply.github.com>
2022-12-19 19:14:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+
+namespace Ödev2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            int puan = 0;    // güçlü mü güçsüz mü diye baktığımız puan değeri
+            bool boslukcontrol = true;   // boşluk var mı diye kontrol edilir.
+            char[] karakterler;
+            Console.Write("Lütfen Şifrenizi Giriniz..:");
+            string girilensifre = Console.ReadLine();
+            karakterler = girilensifre.ToArray();       // girilen şifreyi karakterler dizisine aktarılır
+            
+            
+            Console.WriteLine();
+
+
+            
+            int sayac1 = Test.bykhrf(girilensifre);
+            int sayac2 = Test.kckhrf(girilensifre);
+            int sayac3 = Test.sayi(girilensifre);
+            int sayac4 = Test.symbol(girilensifre);
+            Console.WriteLine();
+
+            Test.sayma(girilensifre);
+            Console.WriteLine();
+
+            for (int i = 0; i < girilensifre.Length; i++)       // dizinin tamamında boşluk var mı diye bakıyor
+            {
+                if (karakterler[i] == ' ')
+                {
+                    boslukcontrol = false;
+                }
+
+            }
+            
+
+            if (boslukcontrol == false)                      
+            {
+                Console.WriteLine("Şifreniz Boşluk içerdiği için geçersizdir. Lütfen Boşluk Kullanmayınız.");
+            }
+            else
+            {
+                if (sayac1 == 0 || sayac2 == 0 || sayac3 == 0 || sayac4 == 0)           // sayı sembol büyük harf ve küçük harf 0 olmasın diye if else kullanılıyor
+                {
+                    Console.Write("Bu Şifrede Sayı Sembol Büyük Harf Küçük Harften Birisi Eksik Olduğundan Kabul Edilemez...");
+                }
+                else
+                {
+                    if (girilensifre.Length < 9)                                       // şifre 9 dan büyük olması isteniyor
+                    {
+                        Console.WriteLine("Bu Şifre Gerekli Uzunlukta Olmadığı İçin Şifre Geçersizdir..");
+                    }
+                    else
+                    {
+                        if (girilensifre.Length == 9)
+                        {
+                            puan = 10;
+                        }
+
+                      
+                        if (sayac1 == 1)
+                        {
+                            puan += 10;
+                        }
+                        else
+                        {
+                            puan += 20;
+
+                        }
+                     
+
+
+                        if (sayac2 == 1)
+                        {
+                            puan += 10;
+                        }
+                        else
+                        {
+                            puan += 20;
+                        }
+
+                        
+
+                        if (sayac3 == 1)
+                        {
+                            puan += 10;
+                        }
+                        else
+                        {
+                            puan += 20;
+                        }
+
+                       
+
+                        if (sayac4 == 1)
+                        {
+                            puan += 10;
+                        }
+                        else if (sayac4 == 2)
+                        {
+                            puan += 20;
+                        }
+                        else if (sayac4 >= 3)
+                        {
+                            puan += 30;
+                        }
+                       
+
+                        if (puan < 70)
+                        {
+                            Console.WriteLine("Şifre Kabul Edilemez...");
+                        }
+                        else if (puan < 90 && puan >= 70)
+                        {
+                            Console.WriteLine("Şifre Kabul Edilir...");
+                        }
+                        else if (puan >= 90 && puan <= 100)
+                        {
+                            Console.WriteLine("Şifreniz Kabul Edildi. Şifreniz:GÜÇLÜ");
+                        }
+                        else
+                        {
+                           
+                        }
+
+
+
+                        Console.WriteLine("Puanınız:"+ puan);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Ödev2
+{
+    public static class Test
+    {
+
+        public static int sayac = 0;     // şifre toplam sayısı 
+        public static int sayac1 = 0;    // büyük harf için
+        public static int sayac2 = 0;    // küçük harf için
+        public static int sayac3 = 0;    // sayı için
+        public static int sayac4 = 0;    // sembol için
+
+        public static int bykhrf(string karakterler)          // şifredeki büyük harfleri bulmak için kullanılan fonksiyon
+        {
+            foreach (char karakter in karakterler)           // dizinin tamamını okusun diye foreach döngüsü kullanılır 
+            {
+                if (char.IsUpper(karakter))                  //  büyük harf var mı diye kontrol ediliyor.
+                {
+                    sayac++;
+                    sayac1++;
+                }
+            }
+            return sayac1;
+        }
+
+
+        public static int kckhrf(string karakterler)          // şifredeki küçük harfleri bulmak için kullanılan fonksiyon
+        {
+            foreach (char karakter in karakterler)           // dizinin tamamını okusun diye foreach döngüsü kullanılır
+            {
+                if (char.IsLower(karakter))                 //  küçük harf var mı diye kontrol ediliyor.
+                {
+                    sayac++;
+                    sayac2++;
+                }
+            }
+            return sayac2;
+        }
+
+
+        public static int sayi(string karakterler)            // şifredeki sayıları bulmak için kullanılan fonksiyon
+        {
+            foreach (char karakter in karakterler)            // dizinin tamamını okusun diye foreach döngüsü kullanılır
+            {
+                if (char.IsNumber(karakter))                 //  sayı var mı diye kontrol ediliyor.
+                {
+                    sayac++;
+                    sayac3++;
+                }
+            }
+            return sayac3;
+        }
+
+
+        public static int symbol(string karakterler)            // şifredeki sembolleri bulmak için kullanılan fonksiyon
+        {
+            foreach (char karakter in karakterler)              // dizinin tamamını okusun diye foreach döngüsü kullanılır
+            {
+                if (!(char.IsLetterOrDigit(karakter)))                    //  sembol var mı diye kontrol ediliyor. Sayı ve Harf değilse Semboldür tanımı yapılır.
+                {
+                    sayac++;
+                    sayac4++;
+                }
+            }
+            return sayac4;
+        }
+
+
+        public static void sayma(string karakterler)           // sayaç değişkenlerimizi ekrana bastırmak için yazılan fonksiyon
+        {
+            Console.WriteLine("Şifredeki Toplam Adet:" + sayac);
+            Console.WriteLine("Büyük Harf Adedi:" + sayac1);
+            Console.WriteLine("Küçük Harf Adedi:" + sayac2);
+            Console.WriteLine("Sayı Adedi:" + sayac3);
+            Console.WriteLine("Sembol Adedi:" + sayac4);
+        }
+    }
+}
